5f3de2ab0d7b09e6add76d5e1ed4547703f2da9a
AgileApps <pradeep.pellakuru@wipro.com>
2018-01-30 12:02:20 +0000

    Project "Sample_ATM" added - ArunKumar

---
+++ Sample_ATM/src/atmTest/AllTests.java
@@ -0,0 +1,11 @@
+package atmTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ atmFunctionTest.class })
+public class AllTests {
+
+}
