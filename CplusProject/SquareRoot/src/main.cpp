7c4b4dfbc54eabcf1c45d45628a4977097ae0338
AgileApps <pradeep.pellakuru@wipro.com>
2018-03-14 13:23:45 +0530

    CPlusPlus Square Root project
    Added  CPlusPlus Square Root project with test cases

---
+++ CplusProject/SquareRoot/src/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"Squarerootfunc.cpp"
+int main()
+{
+int n;
+std::cout<< "Enter the Number"<<std::endl;
+std::cin>> n;
+std::cout<<"Square rooot of "<< n <<" is" << squareRoot(n);
+return 0;
+}
