91daa7f30bc1143cbe1662762f9267182002afc6
AgileApps <pradeep.pellakuru@wipro.com>
2018-05-14 19:48:13 +0530

    added C++ project to test MAKE tool

---
+++ CplusProject/Make_SquareRoot/src/Squarerootfunc.cpp
@@ -0,0 +1,17 @@
+#include <math.h>
+double squareRoot(const double a) {
+    int a1;
+    char b1;
+    float h;
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
+int welcome()
+{
+    int j;
+    return 0;
+ }
---
+++ CplusProject/Make_SquareRoot/src/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"Squarerootfunc.cpp"
+int main()
+{
+int n;
+  char t;
+std::cout<< "Enter the Number"<<std::endl;
+std::cin>> n;
+std::cout<<"Square rooot of "<< n <<" is" << squareRoot(n);
+return 0;
+}
