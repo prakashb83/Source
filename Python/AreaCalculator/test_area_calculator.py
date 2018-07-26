44eec494f215ffe43dbf900830d43d309916c631
AgileApps <arunkumar.sivakumar@wipro.com>
2018-07-26 11:54:25 +0530

    Update test_area_calculator.py

--- Python/AreaCalculator/test_area_calculator.py
+++ Python/AreaCalculator/test_area_calculator.py
@@ -26,4 +26,4 @@ class TestCalc(unittest.TestCase):
        self.assertAlmostEqual(self.a1.circle(5), pi*5**2)	   
 	   
 if __name__ == '__main__':
-    unittest.main(testRunner=HTMLTestRunner(output='Area'))
+    unittest.main(testRunner=HTMLTestRunner(output='UnitTest'))
