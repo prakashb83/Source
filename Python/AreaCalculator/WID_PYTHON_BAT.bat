1e94aee9213afa3f67d8ddbe325ad7a5c43f6aa7
AgileApps <arunkumar.sivakumar@wipro.com>
2018-07-26 16:41:27 +0530

    Update WID_PYTHON_BAT.bat

--- Python/AreaCalculator/WID_PYTHON_BAT.bat
+++ Python/AreaCalculator/WID_PYTHON_BAT.bat
@@ -6,26 +6,26 @@ if /I %1 == securityanalysis goto :securityanalysis
 
 
 :unittest
-python test_area_calculator.py
+python AreaCalculator\test_area_calculator.py
 goto :eof
 
 :codecoverage
-coverage run test_area_calculator.py
+coverage run AreaCalculator\test_area_calculator.py
 coverage html
 goto :eof
 
 :codeanalysis
-pylint area_calculator.py --output-format=json > %2\area_calculator.json
-pylint test_area_calculator.py --output-format=json > %2\test_area_calculator.json
+pylint AreaCalculator\area_calculator.py --output-format=json > %2\area_calculator_ca.json
+pylint AreaCalculator\test_area_calculator.py --output-format=json > %2\test_area_calculator_ca.json
 cd %2
-pylint-json2html -o area_calculator.html area_calculator.json
-pylint-json2html -o test_area_calculator.html test_area_calculator.json
+pylint-json2html -o area_calculator_ca.html area_calculator_ca.json
+pylint-json2html -o test_area_calculator_ca.html test_area_calculator_ca.json
 copy /b *.html codeanalysis.html
 goto :eof
 
 :securityanalysis
-bandit area_calculator.py -f html> %2\area_calculator.html
-bandit test_area_calculator.py -f html > %2\test_area_calculator.html
+bandit AreaCalculator\area_calculator.py -f html> %2\area_calculator_sa.html
+bandit AreaCalculator\test_area_calculator.py -f html > %2\test_area_calculator_sa.html
 cd %2
 copy /b *.html securityanalysis.html
 goto :eof
