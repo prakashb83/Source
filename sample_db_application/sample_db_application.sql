eb2a199cb10311f01055667e62b4fbb8f6f7fa44
AgileApps <arunkumar.sivakumar@wipro.com>
2019-02-12 10:28:39 +0530

    Update sample_db_application.sql

--- sample_db_application/sample_db_application.sql
+++ sample_db_application/sample_db_application.sql
@@ -2,4 +2,3 @@ CREATE DATABASE dbapplication;
 use dbapplication;
 CREATE TABLE users_data(id int primary key auto_increment, name varchar(40), email varchar(50), phone varchar(20));
 INSERT INTO users_data(name, email, phone) values("user1", "user1@gmail.com", "8678904323");
-INSERT INTO users_data(name, email, phone) values("user1=2", "user2@gmail.com", "8678904322");
